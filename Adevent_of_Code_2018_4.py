c638135244a5804819a836c944eb2484f9ea2250
Andrew <andrewwoodcock03@googlemail.com>
2021-01-02 18:21:29 +0000

    AoC 2018 Day 4 part 1 in progress, updated.

--- Adevent_of_Code_2018_4.py
+++ Adevent_of_Code_2018_4.py
@@ -1,11 +1,22 @@
 import pprint
 from datetime import datetime
 
-guard_data = "guard_details.txt"
+# guard_data = "guard_details.txt"
+guard_data = "guard_details_test.txt"
+
+
+class Guard:
+
+    guard_dict = {}
+
+    def __init__(self, ident):
+        self.ident = ident
+        self.tot_sleep_time = []
+        self.sleep_mins = {}
+        Guard.guard_dict[ident] = []
 
 
 def get_file_data(filename) -> list:
-    rex = r"\s+(?=\d{2}(?:\d{2})?-\d{1,2}-\d{1,2}\b)"
     with open(filename, "r") as file:
         entries = []
         for line in file:
@@ -38,12 +49,31 @@ def get_file_data(filename) -> list:
             entries.append(entry)
 
         entries.sort()
-        pprint.pprint(entries)
+        return entries
+
+
+def review_shifts(shift_list: list):
+
+    for i in range(0, len(shift_list)):
+        # check for guard change
+        if shift_list[i][1] is not None:
+            # check if the guard already has an ID, if not create one
+            g_id = shift_list[i][1]
+            if g_id not in Guard.guard_dict:
+                Guard.guard_dict[g_id] = []
+        # check for falling asleep
+        elif shift_list[i][3][1] is True:
+            sleep_t = shift_list[i][0].minute
+        elif shift_list[i][2][1] is True:
+            wake_t = shift_list[i][0].minute
+
+
 
 
 def main():
     shifts = get_file_data(guard_data)
-
+    review_shifts(shifts)
+    print(Guard.guard_dict)
 
 if __name__ == '__main__':
     main()
