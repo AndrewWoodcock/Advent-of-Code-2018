df6ed63a7defa115104254402832ebc987bdfb43
Andrew <andrewwoodcock03@googlemail.com>
2021-01-03 17:46:17 +0000

    AoC 2018 Day 4 part 1 in progress, updated.

--- Adevent_of_Code_2018_4.py
+++ Adevent_of_Code_2018_4.py
@@ -1,8 +1,8 @@
 import pprint
 from datetime import datetime
 
-# guard_data = "guard_details.txt"
-guard_data = "guard_details_test.txt"
+guard_data = "guard_details.txt"
+# guard_data = "guard_details_test.txt"
 
 
 class Guard:
@@ -52,28 +52,69 @@ def get_file_data(filename) -> list:
         return entries
 
 
-def review_shifts(shift_list: list):
+def range_generator() -> list:
+    rng_tup_list = []
+    for i in range(0, 60):
+        rng_tup_list.append([i, 0])
+    return rng_tup_list
 
+
+
+def review_shifts(shift_list: list):
+    g_id = None
     for i in range(0, len(shift_list)):
         # check for guard change
+
         if shift_list[i][1] is not None:
             # check if the guard already has an ID, if not create one
             g_id = shift_list[i][1]
+            print("guard id: {0}".format(g_id))
             if g_id not in Guard.guard_dict:
-                Guard.guard_dict[g_id] = []
+                Guard.guard_dict[g_id] = range_generator()
         # check for falling asleep
         elif shift_list[i][3][1] is True:
             sleep_t = shift_list[i][0].minute
+            print("sleep: {0}".format(sleep_t))
+        # check for waking up
         elif shift_list[i][2][1] is True:
             wake_t = shift_list[i][0].minute
+            print("wake: {0}".format(wake_t))
+            print(g_id)
+            for min in Guard.guard_dict[g_id]:
+                if (min[0] >= sleep_t) and min[0] < wake_t:
+                    min[1] += 1
 
 
+def guard_summary():
+    guard_summary_dict = {}
+    # pull out summary vals
+    for guard, sleep_mins in Guard.guard_dict.items():
+        tot_sleep = len([el[0] for el in sleep_mins if el[1] != 0])
+        guard_summary_dict[guard] = [tot_sleep]
+        mode_sleep = max([el[1] for el in sleep_mins])
+        guard_summary_dict[guard].append(mode_sleep)
+    return guard_summary_dict
+
+
+def part_1_ans(summary: dict):
+    max_guard = [None, 0]
+    for guard, stats in summary.items():
+        if stats[0] > max_guard[1]:
+            max_guard[0] = guard
+            max_guard[1] = stats[0]
+
+    for element in Guard.guard_dict[max_guard[0]]:
+        if element[1] == summary[max_guard[0]][1]:
+            return max_guard[0] * element[0]
 
 
 def main():
     shifts = get_file_data(guard_data)
     review_shifts(shifts)
     print(Guard.guard_dict)
+    # guard_summary()
+    print(part_1_ans(guard_summary()))
+
 
 if __name__ == '__main__':
     main()
